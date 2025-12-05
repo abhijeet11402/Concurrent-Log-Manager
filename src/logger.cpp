40969bdb6fa7eb56f87b412e0811d6d8664571b3
abhijeet11402 <140493087+abhijeet11402@users.noreply.github.com>
2025-12-05 14:54:14 +0530

    Update logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,16 +1,27 @@
 #include "Logger.h"
 #include <iostream>
 
+// Adds a log message to the vector safely using mutex
 void Logger::log(const std::string& message) {
-    std::lock_guard<std::mutex> lock(mtx); // lock mutex while writing
+
+    // Lock the mutex before modifying shared data
+    std::lock_guard<std::mutex> lock(mtx);
+
+    // Store the log message
     log_messages.push_back(message);
 }
 
+// Prints all stored logs safely
 void Logger::printLogs() {
+
+     // Lock the mutex before reading shared data
     std::lock_guard<std::mutex> lock(mtx); // lock mutex while printing
     std::cout << "---- Log Start ----" << std::endl;
+    
+    // Print each stored log message
     for (const auto& msg : log_messages) {
         std::cout << msg << std::endl;
     }
     std::cout << "---- Log End ----" << std::endl;
 }
+
