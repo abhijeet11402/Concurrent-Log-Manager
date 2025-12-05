409bb52a540282654df1a753022d29993a7e8657
abhijeet11402 <140493087+abhijeet11402@users.noreply.github.com>
2025-12-05 14:50:05 +0530

    Update Worker.h

--- includes/Worker.h
+++ includes/Worker.h
@@ -2,11 +2,16 @@
 #include "Logger.h"
 #include <string>
 
+// Worker class represents a thread worker that logs messages
 class Worker {
 private:
-    Logger& logger;
-    std::string name;
+    Logger& logger;        // Shared Logger instance (used by multiple threads)
+    std::string name;    // Name of the worker (e.g., Worker-1)
 public:
+    // Constructor
     Worker(Logger& log, const std::string& workerName);
+
+     // Overloaded function call operator so the object can be used as a thread
     void operator()(); // function call operator for thread
 };
+
